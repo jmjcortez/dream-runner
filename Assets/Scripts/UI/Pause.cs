964eb416a49daadb92a9634d3e9cf98a027b21f3
Jared Cortez <jared.cortez@xref.com>
2020-09-23 23:35:02 +1000

    Updated ui

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public RawImage pauseMenu;
+
+    public GameObject menus, quitConfirmation;
+
+    public void PauseGame() {
+        Time.timeScale = 0;
+        ActivatePrompt();
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1;
+        DeactivatePrompt();
+    }
+
+    public void ConfirmQuit() {
+        menus.SetActive(false);
+        quitConfirmation.SetActive(true);
+    }
+
+    public void CancelQuit() {
+        menus.SetActive(true);
+        quitConfirmation.SetActive(false);
+    }
+    
+    public void ActivatePrompt() {
+        pauseMenu.gameObject.SetActive(true);
+    }
+
+    public void DeactivatePrompt() {
+        pauseMenu.gameObject.SetActive(false);
+    }
+}
